da2170ae198914d694e01a200b802bac3d3a239e
Juanma Cardenas <jmcardenas@correo.iue.edu.co>
2024-11-01 13:40:40 -0500

    carga de base de datos

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import {config} from 'dotenv';
+config();
+
+export default{
+    env: Process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT
+}
+export{config};
