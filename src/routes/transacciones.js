660cb9e3679ac711ac581ed064a287463a6d775e
Juanma Cardenas <jmcardenas@correo.iue.edu.co>
2024-11-01 17:17:35 -0500

    metodos http de prestamos y creacion de carpetas

---
+++ src/routes/transacciones.js
@@ -0,0 +1,9 @@
+// src/routes/transacciones.js
+import { Router } from 'express';
+import { metodosTransacciones } from '../controller/transaccionescontroller.js';
+
+const router = Router();
+
+router.get('/transacciones', metodosTransacciones.getTransacciones);
+
+export default router;
