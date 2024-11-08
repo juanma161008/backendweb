cbffd18f876cf23b7f0098d440c981f2e1265739
Juanma Cardenas <jmcardenas@correo.iue.edu.co>
2024-11-08 17:24:32 -0500

    features erorr

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors'; // Importa cors
-import router from './routes/prestamos.js';
+import router from './routes/usuarios.js';
 
 const app = express();
 app.set('port', 3000);
--- src/database/database.js
+++ src/database/database.js
@@ -1,16 +1,21 @@
 import mysql from 'mysql2/promise';
 import config from '../config.js';
 
-const conection = mysql.createConnection({
+// Crear un pool de conexiones
+const pool = mysql.createPool({
     host: config.dbHost,
     user: config.dbUser,
     password: config.dbPassword,
     database: config.dbName,
-    port: config.dbPort
-})
+    port: config.dbPort,
+    waitForConnections: true,   // Esperar conexiones si el pool está lleno
+    connectionLimit: 10,        // Límite de conexiones simultáneas
+    queueLimit: 0               // No limitar las solicitudes en cola
+});
 
-const getConnection = ()=> {
-    return conection;
+// Función para obtener una conexión del pool
+const getConnection = async () => {
+    return await pool.getConnection();  // Obtiene una conexión del pool
 };
 
-export {getConnection};
+export { getConnection };
